cad8114c6e58dc83c3da05c0a02070df90f99cd1
Angelo_Laptop_ASUS <michaelangelosdr@gmail.com>
2018-01-27 12:37:27 +0800

    Added Main Menu and art assets ready to animate

---
+++ GGJ2018/Assets/GGJSceneManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GGJSceneManager : MonoBehaviour {
+
+
+
+	private static GGJSceneManager instance = null;
+	public static GGJSceneManager Instance
+	{
+		get {
+			return instance;
+		}
+	}
+		
+	void Awake()
+	{
+		instance = this;
+		DontDestroyOnLoad (this.gameObject);
+	}
+
+
+	public void LoadScene(string SceneName)
+	{
+		SceneManager.LoadScene (SceneName);
+	}
+
+}
