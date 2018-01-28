4ec258c7e894cc9e1f7694d5c317567152013854
Tup Luisking <ramon.luisking14@gmail.com>
2018-01-28 13:33:57 +0800

    Splash Screen

---
+++ GGJ2018/Assets/SplashScreenScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SplashScreenScript : MonoBehaviour {
+
+	public Image blackOverlay;
+
+	void Awake() {
+
+		StartCoroutine (SplashingScreen ());
+	}
+
+	IEnumerator SplashingScreen() {
+
+		yield return ChangeColor (Color.black, Color.clear);
+
+		yield return new WaitForSeconds (2);
+
+		yield return ChangeColor (Color.clear, Color.black);
+
+		SceneManager.LoadScene ("Menu_scene");
+	}
+
+	IEnumerator ChangeColor(Color start, Color end) {
+
+		float timeElapsed = 0;
+
+		while (timeElapsed < 1) {
+		
+			blackOverlay.color = Color.Lerp (start, end, timeElapsed / 1);
+			timeElapsed += Time.deltaTime;
+
+			yield return null;
+		}
+
+		blackOverlay.color = end;
+
+		yield return null;
+	}
+}
